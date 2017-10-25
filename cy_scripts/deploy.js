b8ecac148c5068403dcc1f5ea14b08c69e060e02
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-25 10:34:44 -0400

    remove obsolete code

--- cy_scripts/deploy.js
+++ cy_scripts/deploy.js
@@ -1,21 +1,15 @@
 /* eslint-disable no-console */
 
-const _          = require('lodash')
 const path       = require('path')
 const gift       = require('gift')
-// const gulp       = require('gulp')
 const chalk      = require('chalk')
-// const human      = require('human-interval')
 const Promise    = require('bluebird')
 const inquirer   = require('inquirer')
-// const awspublish = require('gulp-awspublish')
-// const parallelize = require('concurrent-transform')
 const minimist   = require('minimist')
 const debug = require('debug')('deploy')
 const questionsRemain = require('@cypress/questions-remain')
 const scrape     = require('./scrape')
 const shouldDeploy = require('./should-deploy')
-// const { configFromEnvOrJsonFile } = require('@cypress/env-or-json-file')
 const R = require('ramda')
 const la = require('lazy-ass')
 const is = require('check-more-types')
@@ -24,9 +18,7 @@ const {
   warnIfNotCI,
   getDeployEnvironment,
   checkBranchEnvFolder,
-  getS3Config,
-  getS3Publisher,
-  publishToS3,
+  uploadToS3,
 } = require('@cypress/deploy-bits')
 
 const distDir = path.resolve('public')
@@ -35,34 +27,9 @@ const isValidEnvironment = is.oneOf(['production', 'staging'])
 // initialize on existing repo
 const repo = Promise.promisifyAll(gift(path.resolve('..')))
 
-// function getS3Credentials () {
-//   const key = path.join('support', '.aws-credentials.json')
-//   const config = configFromEnvOrJsonFile(key)
-//   if (!config) {
-//     console.error('⛔️  Cannot find AWS credentials')
-//     console.error('Using @cypress/env-or-json-file module')
-//     console.error('and key', key)
-//     throw new Error('AWS config not found')
-//   }
-//   return config
-// }
-
 function getCurrentBranch () {
   return git.branchName()
 }
-
-// function promptForDeployEnvironment () {
-//   return prompt({
-//     type: 'list',
-//     name: 'strategy',
-//     message: 'Which environment are you deploying?',
-//     choices: [
-//       { name: 'Staging',    value: 'staging' },
-//       { name: 'Production', value: 'production' },
-//     ],
-//   })
-//   .get('strategy')
-// }
 
 function cliOrAsk (property, ask, minimistOptions) {
   // for now isolate the CLI/question logic
@@ -71,97 +38,6 @@ function cliOrAsk (property, ask, minimistOptions) {
   })
   const options = minimist(process.argv.slice(2), minimistOptions)
   return askRemaining(options).then(R.prop(property))
-}
-
-// const getDeployEnvironment = R.partial(cliOrAsk,
-//   ['environment', promptForDeployEnvironment])
-
-// function ensureCleanWorkingDirectory () {
-//   return repo.statusAsync()
-//   .catch((e) => {
-//     console.error('Could not get Git status')
-//     console.error(e)
-//     console.error('assuming clean status')
-//     return { clean: true }
-//   })
-//   .then((status) => {
-//     if (!status.clean) {
-//       console.log(chalk.red('\nUncommited files:'))
-
-//       _.each(status.files, (props, file) => {
-//         console.log(chalk.red(`- ${file}`))
-//       })
-
-//       console.log('')
-
-//       throw new Error('Cannot deploy master to production. You must first commit these above files.')
-//     }
-//   })
-// }
-
-// function getPublisher (bucket, key, secret) {
-//   return awspublish.create({
-//     httpOptions: {
-//       timeout: human('10 minutes'),
-//     },
-//     params: {
-//       Bucket: bucket,
-//     },
-//     accessKeyId: key,
-//     secretAccessKey: secret,
-//   })
-// }
-
-// function publishToS3 (publisher) {
-//   const headers = {}
-
-//   return new Promise((resolve, reject) => {
-//     const files = path.join(distDir, '**', '*')
-
-//     return gulp.src(files)
-//     .pipe(parallelize(publisher.publish(headers), 100))
-
-//     // we dont need to gzip here because cloudflare
-//     // will automatically gzip the content for us
-//     // after its cached at their edge location
-//     // but we should probably gzip the index.html?
-//     // .pipe(awspublish.gzip({ext: '.gz'}))
-
-//     .pipe(awspublish.reporter())
-//     .on('error', reject)
-//     .on('end', resolve)
-//   })
-// }
-
-// function uploadToS3 (env) {
-//   la(isValidEnvironment(env), 'invalid environment', env)
-//   const bucketName = `bucket-${env}`
-//   return Promise.resolve()
-//   .then(getS3Credentials)
-//   .then((json) => {
-//     la(is.object(json), 'missing S3 credentials object for environment', env)
-//     const bucket = json[bucketName]
-//     la(is.unemptyString(bucket), 'Could not find a bucket for environment', env)
-
-//     console.log('\n', 'Deploying to:', chalk.green(bucket), '\n')
-//     const publisher = getPublisher(bucket, json.key, json.secret)
-//     return publisher
-//   })
-//   .then(publishToS3)
-// }
-
-function uploadToS3 (env) {
-  la(is.unemptyString(env), 'missing S3 environment', env)
-  const config = getS3Config()
-  const bucket = config[`bucket-${env}`]
-  la(is.unemptyString(bucket), 'Could not find a bucket for environment', env)
-  console.log('')
-  console.log('Deploying to:', chalk.green(bucket))
-  console.log('')
-
-  const publisher = getS3Publisher(bucket, config.key, config.secret)
-  la(publisher, 'could not get publisher for bucket', bucket)
-  return publishToS3(distDir, publisher)
 }
 
 function prompt (questions) {
@@ -235,25 +111,6 @@ function deployEnvironmentBranch (env, branch) {
   la(is.unemptyString(branch), 'missing branch to deploy', branch)
   la(isValidEnvironment(env), 'invalid deploy environment', env)
 
-  // const cleanup = () => {
-  //   console.log('Target environment:', chalk.green(env))
-  //   console.log('On branch:', chalk.green(branch), '\n')
-  //   if (env === 'staging') {
-  //     return env
-  //   }
-
-  //   if (env === 'production') {
-  //     if (branch !== 'master') {
-  //       throw new Error('Cannot deploy master to production. You are not on the \'master\' branch.')
-  //     }
-
-  //     return ensureCleanWorkingDirectory()
-  //   } else {
-  //     throw new Error(`Unknown environment: ${env}`)
-  //   }
-  // }
-
-  const uploadEnvToS3 = _.partial(uploadToS3, env)
   const maybeCommit = () =>
     commitMessage(env, branch)
     .catch((err) => {
@@ -262,8 +119,9 @@ function deployEnvironmentBranch (env, branch) {
       console.error(err.message)
     })
 
-  return checkBranchEnvFolder(branch)(env)
-  .then(uploadEnvToS3)
+  checkBranchEnvFolder(branch)(env)
+
+  uploadToS3(distDir, env)
   .then(maybeCommit)
   .then(() => scrapeDocs(env, branch))
   .then(() => {
@@ -278,7 +136,8 @@ function doDeploy (env) {
   debug('getting current branch')
   return getCurrentBranch()
     .then((branch) => {
-      console.log('deploying branch %s to %s', branch, env)
+      console.log('deploying branch %s to environment %s',
+        chalk.green(branch), chalk.blue(env))
       la(is.unemptyString(branch), 'invalid branch name', branch)
       return deployEnvironmentBranch(env, branch)
     })
