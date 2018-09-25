f49cadb8062e0aed28745bc5d0cc37c2660bee64
Jennifer Shehane <jennifer@cypress.io>
2018-09-25 02:11:15 -0400

    Have toc stay within view at all times and remain independently scrollable
    - may have messed up menuspy’s tracking of scrolling.

--- themes/cypress/source/js/toc.js
+++ themes/cypress/source/js/toc.js
@@ -35,23 +35,6 @@
     ms.tick()
   })
 
-  // This keeps the toc within the view
-  function updateSidebarPosition () {
-    var scrollTop = document.scrollingElement.scrollTop
-
-    if (scrollTop > headerHeight) {
-      toc.classList.add('fixed')
-    } else {
-      toc.classList.remove('fixed')
-    }
-  }
-
-  window.addEventListener('scroll', function () {
-    window.requestAnimationFrame(updateSidebarPosition)
-  })
-
-  updateSidebarPosition()
-
   tocTop.addEventListener('click', function (e) {
     e.preventDefault()
     document.scrollingElement.scrollTop = 0
