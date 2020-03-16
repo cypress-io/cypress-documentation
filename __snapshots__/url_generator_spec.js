e698787f441cf1d5fb3324b75588ef1057a81ed5
Cecelia Martinez <cecelia@cypress.io>
2020-03-16 13:44:12 -0400

    restores final Newline to files changed in previous commit.

--- __snapshots__/url_generator_spec.js
+++ __snapshots__/url_generator_spec.js
@@ -5,3 +5,4 @@ exports['lib/url_generator .getLocalFile throws when cannot find file 1'] = `Con
     > Could not find a valid doc file in the sidebar.yml for: "foo"
       the full url was "foo"
     `
+    
