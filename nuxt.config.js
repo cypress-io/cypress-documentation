a1a36a51f4f17f79e1bd0f61a1a3d5f8caf47f8d
Matt Hamil <j.matthew.hamil@gmail.com>
2021-03-05 16:36:26 -0600

    fix: only include GTM env var during production builds

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,7 +83,8 @@ export default {
    ** Google Tag Manager
    */
   gtm: {
-    id: process.env.GOOGLE_TAG_MANAGER_ID || 'GTM-XXXXXXX',
+    // The env var CONTEXT is set by Netlify and can be 'production', 'deploy-preview', or 'branch-deploy'
+    id: (process.env.CONTEXT === 'production' && process.env.GOOGLE_TAG_MANAGER_ID) || 'GTM-XXXXXXX',
   },
   /*
    ** Axios module configuration
