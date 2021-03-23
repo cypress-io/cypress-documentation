38845b0cb8b69256ac8dc65e25368c4caa7e9b12
Matt Hamil <j.matthew.hamil@gmail.com>
2021-03-23 15:01:59 -0500

    fix: remove trailing slashes from URL

--- static/js/removeTrailingSlash.js
+++ static/js/removeTrailingSlash.js
@@ -1,4 +1,17 @@
 ;(function removeTrailingSlash() {
+  if (window.location.pathname === '/') {
+    // do nothing, otherwise this will cause
+    // an infinite number of redirects
+    return
+  }
+
+  /**
+   * Netlify is allowing for paths to end with `/`, however, this can cause issues
+   * if a user lands on a URL that ends with `/`, navigates to another page,
+   * then presses the browser's back button. The browser's address bar will update
+   * to the previous URL, but the page will 404. To eliminate this possibility,
+   * we will remove any trailing slashes from the window.location.pathname.
+   */
   if (window.location.pathname[window.location.pathname.length - 1] === '/') {
     let newLocation =
       window.origin +
