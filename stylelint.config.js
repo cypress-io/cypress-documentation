49f0bec4a76c3d7e49667f05a7fd2af316d76b76
Matt Hamil <matthamil@users.noreply.github.com>
2021-04-12 13:33:55 -0500

    style: migrate <Alert> to css modules from scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-prettier',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'no-descending-specificity': null,
+  },
 }
