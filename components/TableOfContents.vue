a143e8e5ff719ea86f03e6c22ffe0e7307990cbe
Matt Hamil <matthamil@users.noreply.github.com>
2021-04-12 12:57:21 -0500

    style: migrate TOC and Badge to css modules

--- components/TableOfContents.vue
+++ components/TableOfContents.vue
@@ -4,11 +4,11 @@
       v-if="toc.length"
       class="fixed w-sidebar top-16 bottom-0 right-0 pl-4 pr-8 pb-8 overflow-y-auto space-y-4 hide-scroll"
     >
-      <h3 class="toc-heading">ON THIS PAGE</h3>
+      <h3 :class="$style.tocHeading">ON THIS PAGE</h3>
       <scrollactive
         :scroll-on-start="false"
         highlight-first-item
-        active-class="scrollactive-item-active"
+        :active-class="$style.scrollactiveItemActive"
         :offset="100"
         tag="ul"
       >
@@ -56,17 +56,17 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style module>
 .toc {
   @apply pl-8;
   @apply pr-2;
-
-  li {
-    @apply list-none;
-  }
 }
 
-.toc-heading {
+.toc li {
+  @apply list-none;
+}
+
+.tocHeading {
   @apply mb-2;
   @apply pt-5;
   @apply text-gray-500;
@@ -76,18 +76,7 @@ export default {
   @apply text-xs;
 }
 
-.toc-nav {
-  top: 0;
-  height: calc(100vh - 35px);
-  @apply py-4;
-}
-
-.scrollactive-nav {
-  @apply mb-6;
-  @apply pb-8;
-}
-
-.scrollactive-item-active {
+.scrollactiveItemActive {
   border-color: #04c38d;
   @apply text-gray-500;
   @apply font-bold;
