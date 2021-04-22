1c0fe052fea348d98514a845d32977ffa5e757d8
Matt Hamil <matthamil@users.noreply.github.com>
2021-04-22 13:49:02 -0500

    fix: code-group and code-block style regression

---
+++ components/global/CodeBlock.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<template>
+  <div :class="[active && 'active', $style.codeBlock]">
+    <slot />
+  </div>
+</template>
+
+<style module>
+.codeBlock {
+  display: none;
+}
+
+:global(.active) {
+  display: block;
+}
+
+:global(.active .line-numbers) {
+  @apply mt-0;
+}
+</style>
