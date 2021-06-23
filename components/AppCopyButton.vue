c10e597990db5b9395b7a008e778bff0b3f4067d
Adam Murray <adamm@cypress.io>
2021-06-23 13:33:26 -0500

    fix: update copy button to include text and hover for visibility and accessibility. Move styles into component.

--- components/AppCopyButton.vue
+++ components/AppCopyButton.vue
@@ -1,7 +1,12 @@
 <template>
-  <button ref="copy" class="copy" data-test="copy-button">
-    <Icon v-if="state === 'copied'" name="check-circle" color="green" data-test="copy-button-copied" />
-    <Icon v-else name="paste" color="inherit" data-test="copy-button-default" class="w-5 h-5" />
+  <button
+    ref="copy"
+    :class="$style.copy"
+    class="copy rounded-t-md rounded-b-md text-sm text-white font-normal relative flex flex-row"
+    data-test="copy-button"
+  >
+    <Icon :name="state === 'copied' ? 'check-circle' : 'paste'" color="white" />
+    <span>{{ state === 'copied' ? 'Copied' : 'Copy' }}</span>
   </button>
 </template>
 
@@ -13,24 +18,42 @@ export default {
   components: {
     Icon,
   },
-  data () {
+  data() {
     return {
-      state: 'init'
+      state: 'init',
     }
   },
-  mounted () {
+  mounted() {
     const copyCode = new Clipboard(this.$refs.copy, {
-      target (trigger) {
+      target(trigger) {
         return trigger.parentElement
-      }    })
+      },
+    })
 
     copyCode.on('success', (event) => {
       event.clearSelection()
       this.state = 'copied'
       window.setTimeout(() => {
         this.state = 'init'
-      }, 2000)
+      }, 1500)
     })
-  }
+  },
 }
 </script>
+
+<style module>
+.copy {
+  border: 1px solid transparent;
+  margin-left: 1rem;
+  padding: 0.25rem 0.75rem;
+  align-items: center;
+}
+
+.copy svg {
+  margin-right: 0.5rem;
+}
+
+.copy:hover {
+  border: 1px solid #ddd;
+}
+</style>
