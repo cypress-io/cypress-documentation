b97e41f93c7bd550690ade4a834764c14a50ab3e
Ben Alman <cowboy@rj3.net>
2021-11-19 12:19:29 -0500

    Fix whitespace issues

--- scripts/directives/visit-mount-test-example.js
+++ scripts/directives/visit-mount-test-example.js
@@ -1,21 +1,22 @@
 const re = /( *)__VISIT_MOUNT_PLACEHOLDER__/g
 const replacer = (text) => {
-	return (_, padding) => {
-	return text
-		.split('\n')
-		.map((s) => `${padding}${s}`)
-		.join('\n')
-}}
+  return (_, padding) => {
+    return text
+      .split('\n')
+      .map((s) => `${padding}${s}`)
+      .join('\n')
+  }
+}
 
 function processNode(node, { _require, error, warn }) {
   const helpers = _require(__dirname, './helpers/example-helpers')
   const { children } = helpers.getNodeProperties(node)
   const { errorArgs, parts } = helpers.getCodeBlocks(children, { count: 3 })
-    
+
   if (errorArgs) {
     return error(...errorArgs)
   }
-    
+
   const [visit, mount, code] = parts
   const visitCombined = code.replace(re, replacer(visit))
   const mountCombined = code.replace(re, replacer(mount))
@@ -24,12 +25,12 @@ function processNode(node, { _require, error, warn }) {
     {
       label: 'End-to-End Test',
       language: 'js',
-      body: visitCombined
+      body: visitCombined,
     },
     {
       label: 'Component Test',
       language: 'js',
-      body: mountCombined
+      body: mountCombined,
     }
   )
 }
