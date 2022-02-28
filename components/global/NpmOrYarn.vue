be1f6c0927d94324d4119512a37457af0a8890cd
Ely Lucas <ely@meta-tek.net>
2022-02-28 13:55:39 -0700

    updates to framework config doc

---
+++ components/global/NpmOrYarn.vue
@@ -0,0 +1,12 @@
+<template>
+  <CodeGroup sync-group="npm-or-yarn">
+    <CodeBlock label="npm" active>
+      <template #info><slot name="npm-info"></slot></template>
+      <slot name="npm"></slot>
+    </CodeBlock>
+    <CodeBlock label="yarn">
+      <template #info><slot name="yarn-info"></slot></template>
+      <slot name="yarn"></slot>
+    </CodeBlock>
+  </CodeGroup>
+</template>
