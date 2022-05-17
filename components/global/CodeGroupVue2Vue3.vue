7aaf27ec4578ba6405d6e2c3c1b94b08819557cc
Ely Lucas <ely@meta-tek.net>
2022-05-17 06:52:07 -0600

    making vue3 first in vue2-vue3 code group

--- components/global/CodeGroupVue2Vue3.vue
+++ components/global/CodeGroupVue2Vue3.vue
@@ -1,12 +1,12 @@
 <template>
   <CodeGroup sync-group="vue2-vue3">
-    <CodeBlock label="Vue 2" active>
-      <template #alert><slot name="vue2-alert"></slot></template>
-      <slot name="vue2"></slot>
-    </CodeBlock>
-    <CodeBlock label="Vue 3">
+    <CodeBlock label="Vue 3" active>
       <template #alert><slot name="vue3-alert"></slot></template>
       <slot name="vue3"></slot>
     </CodeBlock>
+    <CodeBlock label="Vue 2">
+      <template #alert><slot name="vue2-alert"></slot></template>
+      <slot name="vue2"></slot>
+    </CodeBlock>    
   </CodeGroup>
 </template>
