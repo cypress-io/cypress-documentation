dca2f9cad4c87932823dfd1e764d51b42dacbacc
DEBRIS APRON <debrisapron@gmail.com>
2022-06-01 13:37:40 -0700

    Cleanup

--- cypress.config.js
+++ cypress.config.js
@@ -7,8 +7,6 @@ module.exports = defineConfig({
     openMode: 0,
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents() {},
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.spec.js',
