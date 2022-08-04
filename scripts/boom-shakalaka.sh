c4380b30e005250cab4170cef4ebafcb505ee9e8
Robert Guss <robertguss@pm.me>
2022-08-04 10:07:25 -0400

    added lowercase script to boom shakalaka bash script

--- scripts/boom-shakalaka.sh
+++ scripts/boom-shakalaka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# node ./scripts/convertToMDX.js &&
+node ./scripts/convertToMDX.js &&
 node ./scripts/replaceAlertComponents.js &&
 node ./scripts/replaceCodeGroupComponents.js &&
 node ./scripts/replaceCodeGroupNPMYarn.js &&
@@ -8,4 +8,5 @@ node ./scripts/replaceCodeGroupReactVue.js &&
 node ./scripts/replaceCodeGroupReactVue2Vue3.js &&
 node ./scripts/replaceCypressConfigFileTabs.js &&
 node ./scripts/replaceIncludePartials.js &&
-node ./scripts/removeNavGuideComponents.js
+node ./scripts/removeNavGuideComponents.js &&
+node ./scripts/lowercaseInternalLinks.js
