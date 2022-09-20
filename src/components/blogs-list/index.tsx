5d1b235a5cf667c619f049a5bcdab42eed52e258
Paul Jaffre <jaffrepaul@gmail.com>
2022-09-20 13:48:15 -0400

    fix media layout

--- src/components/blogs-list/index.tsx
+++ src/components/blogs-list/index.tsx
@@ -8,8 +8,8 @@ export default function blogsList(): BlogsProps {
   return (
     <>
       <div className="main-content-article-wrapper">
-        <article className="main-content-article hide-scroll nuxt-content">
-          <div className="mb-14">
+        <article>
+          <div>
             <ul>
               {blogsJSON.small.map((blog, index) => (
                 <li key={index}>
@@ -29,20 +29,17 @@ export default function blogsList(): BlogsProps {
             <div className="row">
               {blogsJSON.large.map((blog, index) => (
                 <div key={index} className="col col--6 margin-bottom--xl">
-                  <div className="relative mb-4 h-20">
-                    <a
-                      href={blog.sourceUrl}
-                      className="text-xl font-bold no-underline border-none absolute"
-                    >
-                      <h3> {blog.title}</h3>
+                  <div>
+                    <a href={blog.sourceUrl}>
+                      <h3 className={s.contentHeader}>{blog.title}</h3>
                     </a>
                   </div>
                   <a href={blog.sourceUrl}>
-                    <img src={blog.img} alt={`${blog.title}`} />
+                  {blog.img ? <img width="340" height="220" src={blog.img} alt={`${blog.title}`} /> : null}
                   </a>
                   <p>
                     Published on <a href={blog.sourceUrl}>{blog.sourceName}</a>{" "}
-                    by {blog.author}, <em>{blog.date}</em>
+                    by {blog.author} <em>({blog.date})</em>
                   </p>
                 </div>
               ))}
--- src/components/screencasts-list/index.tsx
+++ src/components/screencasts-list/index.tsx
@@ -8,8 +8,8 @@ export default function ScreencastsList(): ScreencastProps {
   return (
     <>
       <div className="main-content-article-wrapper">
-        <article className="main-content-article hide-scroll nuxt-content">
-          <div className="mb-14">
+        <article>
+          <div className={s.container}>
             <ul>
               {screencastsJSON.small &&
                 screencastsJSON.small.map((talk, index) => (
@@ -19,58 +19,52 @@ export default function ScreencastsList(): ScreencastProps {
                 ))}
             </ul>
           </div>
+          <div className={s.container}>
+            {screencastsJSON.large.map((talk, index) => (
+            <div className={s.column} key={index}>
+              <div>
+                <a
+                  href={
+                    talk.youtubeId
+                      ? `https://www.youtube.com/watch?v=${talk.youtubeId}`
+                      : talk.url
+                  }
+                >
+                  <h3 className={s.contentHeader}>{talk.title}</h3>
+                </a>
+              </div>
+              <div>
+                {talk.youtubeId && (
+                  <iframe
+                    src={`https://www.youtube.com/embed/${talk.youtubeId}`}
+                    frameBorder="0"
+                    allow="autoplay; encrypted-media"
+                    allowFullScreen
+                  ></iframe>
+                )}
 
-          <div className="container">
-            <div className="row">
-              {screencastsJSON.large.map((talk, index) => (
-                <div key={index} className="col col--6 margin-bottom--xl">
-                  <div className="relative mb-4 h-20">
-                    <a
-                      href={
-                        talk.youtubeId
-                          ? `https://www.youtube.com/watch?v=${talk.youtubeId}`
-                          : talk.url
-                      }
-                      className="text-xl font-bold no-underline border-none absolute"
-                    >
-                      <h3>{talk.title}</h3>
-                    </a>
-                  </div>
-
-                  <div className="mt-4">
-                    {talk.youtubeId && (
-                      <iframe
-                        src={`https://www.youtube.com/embed/${talk.youtubeId}`}
-                        frameBorder="0"
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen
-                      ></iframe>
-                    )}
-
-                    {talk.img && (
-                      <a href={talk.sourceUrl}>
-                        <img src={talk.img} alt={`${talk.title}`} />
-                      </a>
-                    )}
-                  </div>
-
-                  <p className="mt-8 mb-8">
-                    Published on <a href={talk.sourceUrl}>{talk.sourceName}</a>{" "}
-                    by {talk.author} <em>({talk.date})</em>.
-                    {talk.slides && (
-                      <a
-                        href={talk.slides}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {" "}
-                        Slides.
-                      </a>
-                    )}
-                  </p>
-                </div>
-              ))}
+                {talk.img && (
+                  <a href={talk.sourceUrl}>
+                    <img src={talk.img} alt={`${talk.title}`} />
+                  </a>
+                )}
+              </div>
+              <p>
+                Published on <a href={talk.sourceUrl}>{talk.sourceName}</a>{" "}
+                by {talk.author} <em>({talk.date})</em>.
+                {talk.slides && (
+                  <a
+                    href={talk.slides}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    Slides.
+                  </a>
+                )}
+              </p>
             </div>
+            ))}
           </div>
         </article>
       </div>
