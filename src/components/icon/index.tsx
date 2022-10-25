72cc84c444413c6aa3e19d733707a5d6ac3de5ce
Paul Jaffre <jaffrepaul@gmail.com>
2022-10-25 19:13:03 -0400

    update icon issue comment for clarity

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-// Note: Docusaurus/MDX has parsing issues and some markdown renders statically when beside a component or HTML tag while NOT in a section header or table, etc. https://github.com/facebook/docusaurus/discussions/6886#discussioncomment-2328085 For now, we we wrap the anchor in this component for reusability.
+/* Note: Docusaurus/MDX v1 has parsing issues and some markdown renders statically when beside a component or
+HTML tag while NOT in a section header or table, etc. https://github.com/mdx-js/mdx/issues/1571#issuecomment-853384939
+
+Exact issue here is noted here https://docusaurus.io/docs/markdown-features/react#markdown-and-jsx-interoperability. 
+
+This should be fixed when DS moves to MDX v2 https://github.com/facebook/docusaurus/issues/4029
+
+For now, we we wrap the anchor in this component for reusability. Other in-line workarounds are add &nbsp; before the <Icon />
+or replace markdwon syntax with equivalient html tags. */
+
+// TODO: refactor this component after DS moves to MDX v2.
 
 export default function Icon({ name, color, inline, url, callout, contentType }) {
   const iconName = name === "github" ? ["fab", "github"] : name;
