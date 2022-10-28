16abd2eba7baa8748b8346fe1e42dfa986c6ad8d
Paul Jaffre <jaffrepaul@gmail.com>
2022-10-28 17:46:37 -0400

    lite YT WIP

--- src/components/video-youtube/index.tsx
+++ src/components/video-youtube/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { VideoYouTubeProps } from "./types";
 
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
+
 export default function VideoYouTube({ src, title }: VideoYouTubeProps) {
+  const videoId = () => {
+    const idStartIndex = src.lastIndexOf("/") + 1;
+
+    return src.slice(idStartIndex);
+  };
+
   return (
+    // <div className="embedContainer">
+    //   <iframe src={src} title={title} allowFullScreen></iframe>
+    // </div>
     <div className="embedContainer">
-      <iframe src={src} title={title} allowFullScreen></iframe>
+      <LiteYouTubeEmbed 
+        id={videoId()} title={title}
+      />
     </div>
+
   );
 }
