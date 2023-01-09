7279f6f4794f4d6b70c2e55192a955c7324bcfe4
Paul Jaffre <jaffrepaul@gmail.com>
2023-01-09 10:36:34 -0500

    deeplink top banner to v10 migration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -138,7 +138,7 @@ const config = {
       // optional, can be commented out
       announcementBar: {
         content:
-          '🚀 &nbsp;Looking to move to Cypress v10? Check out the <a target="_blank" rel="noopener noreferrer" href="/guides/references/migration-guide">migration guide!</a>',
+          '🚀 &nbsp;Looking to move to Cypress v10? Check out the <a target="_blank" rel="noopener noreferrer" href="/guides/references/migration-guide#Migrating-to-Cypress-100">migration guide!</a>',
         backgroundColor: '#fff3b9',
         textColor: '#091E42',
         isCloseable: true,
