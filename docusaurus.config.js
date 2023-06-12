d5e70cb2b0da6605dd736335f869a63c5c4fb2bf
Paul Jaffre <jaffrepaul@gmail.com>
2023-06-12 18:05:38 -0400

    revert broken links to throw during build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,8 +19,8 @@ const config = {
     'Fast, easy and reliable testing for anything that runs in a browser.',
   url: 'https://docs.cypress.io',
   baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
   favicon: undefined,
 
   // Even if you don't use internalization, you can use this field to set useful
