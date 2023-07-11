77edc6cf93713ee25c7c009309abca17913c8716
paulj <jaffrepaul@gmail.com>
2023-07-11 12:03:58 -0400

    remove conditional frontmatter logic & normalize styles

--- src/theme/TOCInline/index.tsx
+++ src/theme/TOCInline/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import TOCInline from '@theme-original/TOCInline';
-import {useDoc} from '@docusaurus/theme-common/internal';
 
 export default function TOCInlineWrapper(props) {
-  const { frontMatter: {inlineErrorsTable} } = useDoc();
-  const inlineTOCErrorStyle = inlineErrorsTable &&
-    ["inlineTOC", "errorsInlineTOC"].join(' ');
 
   return (
-    <>
-      <div className={inlineTOCErrorStyle || "inlineTOC"}>
-        <TOCInline {...props} />
-      </div>
-    </>
+    <div className="inlineTOC">
+      <TOCInline {...props} />
+    </div>
   );
 }
