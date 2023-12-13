d6ebefb76324cc54a68cc6ffd15210c3ee9db582
elevatebart <bart@cypress.io>
2023-12-13 17:12:54 +0100

    some updates to dark mode

--- src/theme/TOCItems/index.js
+++ src/theme/TOCItems/index.js
@@ -4,7 +4,7 @@ import TOCItems from '@theme-original/TOCItems'
 export default function TOCItemsWrapper(props) {
   return (
     <>
-      <h2 className="border-l border-gray-100 my-0 mb-[-8px] pb-[12px] pl-[40px] text-gray-500 font-medium uppercase text-[16px]">
+      <h2 className="border-l border-gray-100 dark:border-gray-900 my-0 mb-[-8px] pb-[12px] pl-[40px] text-gray-500 font-medium uppercase text-[16px]">
         Contents
       </h2>
       <TOCItems {...props} />
