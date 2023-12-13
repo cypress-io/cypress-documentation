3f2b24a9b8989d58320480cf9b0c9c29563a51ea
elevatebart <bart@cypress.io>
2023-12-13 17:32:20 +0100

    remove negative margin in toc

--- src/theme/TOCItems/index.js
+++ src/theme/TOCItems/index.js
@@ -4,10 +4,13 @@ import TOCItems from '@theme-original/TOCItems'
 export default function TOCItemsWrapper(props) {
   return (
     <>
-      <h2 className="border-l border-gray-100 dark:border-gray-900 my-0 mb-[-8px] pb-[12px] pl-[40px] text-gray-500 font-medium uppercase text-[16px]">
+      <h2 className="border-l border-gray-100 dark:border-gray-900 my-0 pb-[12px] pl-[40px] text-gray-500 font-medium uppercase text-[16px]">
         Contents
       </h2>
-      <TOCItems {...props} />
+      <TOCItems
+        {...props}
+        className="py-[.1rem] table-of-contents table-of-contents__left-border"
+      />
     </>
   )
 }
