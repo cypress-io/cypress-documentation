c588d991019f0ed1d76bf186f470176912d1b2ea
elevatebart <bart@cypress.io>
2023-12-14 19:53:16 +0100

    two css fixes

--- src/theme/BackToTopButton/index.js
+++ src/theme/BackToTopButton/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DarkModeSwitch from './DarkModeSwitch'
 export default function BackToTopButton() {
   return (
-    <div className="fixed bottom-[1rem] right-[6rem]">
+    <div className="fixed bottom-[1rem] right-[6rem] z-[100]">
       <DarkModeSwitch />
     </div>
   )
