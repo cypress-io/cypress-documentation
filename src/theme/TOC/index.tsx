4f346361008ac7576f73eb6c8c1d922aa879b270
elevatebart <bart@cypress.io>
2023-12-18 17:41:40 +0100

    adjustments on toc

---
+++ src/theme/TOC/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import clsx from 'clsx'
+import TOC from '@theme-original/TOC'
+
+export default function TOCWrapper(props) {
+  return (
+    <>
+      <h2 className="border-l border-gray-100 dark:border-gray-900 my-0 pb-[12px] pl-[3rem] text-gray-500 font-medium uppercase text-[16px]">
+        Contents
+      </h2>
+      <TOC
+        {...props}
+        className={clsx(
+          props.className,
+          'table-of-contents__left-border pt-[.1rem] table-of-contents'
+        )}
+      />
+    </>
+  )
+}
