a1f7cd5cbfd9dd194ea41b38fd0413511dfba46a
Jennifer Shehane <jennifer@cypress.io>
2024-10-23 13:31:10 -0400

    update config

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,14 +14,14 @@ export default defineConfig({
         "createFileTree"({ path }) {
           // take the given path and create an array of all file paths
           // with each files and the directory path of the file as strings
-          // for example: given 'docs/accessibility' return
-          // ['docs/accessibility/get-started/introduction', 'docs/accessibility/core-concepts/how-it-works']
+          // for example: given 'accessibility' return
+          // ['accessibility/get-started/introduction', 'accessibility/core-concepts/how-it-works']
           path = 'docs/' + path;
 
-          function walk(dir) {
+          function walk(dir: string): string[] {
             return readdirSync(dir, { withFileTypes: true }).flatMap((file) => {
               if (file.name.includes('_category_.json') || file.name.includes('.DS_Store')) {
-                return
+                return []
               }
 
               if (file.name.includes('.mdx')) {
@@ -32,47 +32,12 @@ export default defineConfig({
               if (file.isDirectory()) {
                 return walk(join(dir, file.name))
               } else {
-                // remove the 'docs' string from the dir
-                // dir = dir.slice(5)
-
-                return join(dir, file.name)
+                return [join(dir, file.name)]
               }
             })
           }
 
           return walk(path).filter((file) => file !== undefined).map((file) => file.slice(5))
-
-// return walk(path)
-
-        //   async function makeFileTree(path: string) {
-        //     try {
-        //       const files = await readdir(path);
-        //       for (const file of files) {
-        //         console.log('isDir', path, fs.statSync(path).isDirectory())
-        //         if (fs.statSync(path + '/' + file).isDirectory()) {
-        //           makeFileTree(path + '/' + file);
-        //         } else {
-        //           // if (file === '_category_.json') {
-        //           //   return
-        //           // }
-        //           console.log(path + '/' + file);
-        //           fileTree.push(path + '/' + file);
-        //         }
-        //       }
-        //     } catch (err) {
-        //       // console.error(err);
-        //     } 
-        //   }
-
-        //   async function getFiles(path: string) {
-        //     makeFileTree(path);
-        //     console.log('FILETREE', fileTree)
-
-        //     return fileTree
-        //   }
-
-        //   return getFiles(path);
-
         }
       })
       }
