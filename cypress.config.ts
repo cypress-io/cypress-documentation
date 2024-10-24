a37503e981eea39166d9b04e0b852e8803866063
Jennifer Shehane <jennifer@cypress.io>
2024-10-24 09:42:06 -0400

    remove unnecessary folders

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ import { join } from 'path'
 
 export default defineConfig({
   projectId: 'imown1',
+  fixturesFolder: false,
   viewportHeight: 800,
   viewportWidth: 1200,
   experimentalMemoryManagement: true,
