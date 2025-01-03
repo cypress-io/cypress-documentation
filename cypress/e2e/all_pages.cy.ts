e0d3792f6c648eb179e22600eb91c6d359c6cfd0
Jennifer Shehane <jennifer@cypress.io>
2025-01-03 14:15:56 -0500

    Ensure the page visited is not the 404 page

--- cypress/e2e/all_pages.cy.ts
+++ cypress/e2e/all_pages.cy.ts
@@ -5,7 +5,10 @@ describe('Visit all pages', () => {
   URLs.forEach((URL) => {
     it(`Visit ${URL} `, () => {
       cy.visit(URL)
-      cy.get('h1').should('be.visible')
+      cy.get('h1')
+        .should('be.visible')
+        .and('not.have.text', 'Page Not Found')
+        
       cy.get('[aria-label="Switch to dark mode"]').click()
     })
   })
